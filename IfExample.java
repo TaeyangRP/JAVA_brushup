15f9994f5ee216aa608a6fed3b7c4cc58651fbe0
yeo taeyang <rhapersephone@gmail.com>
2022-12-13 08:09:31 +0900

    if && if

--- IfExample.java
+++ IfExample.java
@@ -5,9 +5,16 @@ public class IfExample {
     public static void main(String[] args) throws Exception {
         String x = "R'ha";
         String y = "Persephone";
+        String z = "Hades";
 
-        if (x == "R'ha" && y == "Persephone")  {
-            System.out.println("R'ha Persephone");
+//        if (x == "R'ha" && y == "Persephone")  {
+ //           System.out.println("R'ha Persephone");
+ //       }
+
+        if (x == "R'ha" && y == "Persephone") {
+            if(z == "Hades") {
+                System.out.println("Hades and Persephone");
+            }
         }
         
     }
