9c15a91a3e9a41841e61efee5acbfdba6b728d1b
yeo taeyang <rhapersephone@gmail.com>
2022-12-13 05:24:57 +0900

    If 연습

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 10;
+
+        if(x<10) {
+            System.out.println("뭔가 해보고 있다.");
+        }
+    }
+}
---
+++ IfExample.java
@@ -0,0 +1,12 @@
+import javax.sql.rowset.spi.SyncResolver;
+
+public class IfExample {
+
+    public static void main(String[] args) throws Exception {
+        int x = 10;
+
+        if(x<20) {
+            System.out.println("뭔가 해보고 있다.");
+        }
+    }
+}
