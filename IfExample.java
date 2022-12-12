780c6998da35a1aaf0cb5b987b8881078c7e53bd
yeo taeyang <rhapersephone@gmail.com>
2022-12-13 05:44:45 +0900

    Initial commit

--- IfExample.java
+++ IfExample.java
@@ -3,13 +3,17 @@ import javax.sql.rowset.spi.SyncResolver;
 public class IfExample {
 
     public static void main(String[] args) throws Exception {
-        int x = 10;
+        int x = 40;
 
         if(x<10) {
             System.out.println("x가 10 미만이면 출력된다.");
+        } else if(x<20) {
+            System.out.println("x가 20 미만이면 출력된다.");
+        } else if(x<30) {
+            System.out.println("x가 30 미만이면 출력된다.");
         } else {
-            System.out.println("그렇지 않을 경우 출력된다.");
-
+            System.out.println("x가 30 이상이면 출력된다.");
         }
+        System.out.println("gitHub 업로드 연습을 겸한다.");
     }
 }
