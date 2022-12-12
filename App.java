96d63a45a5859083825280a1028fc029940103b5
yeo taeyang <rhapersephone@gmail.com>
2022-12-13 05:33:34 +0900

    If else 연습

--- App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        int x = 10;
-
-        if(x<10) {
-            System.out.println("뭔가 해보고 있다.");
-        }
-    }
-}
--- IfExample.java
+++ IfExample.java
@@ -5,8 +5,11 @@ public class IfExample {
     public static void main(String[] args) throws Exception {
         int x = 10;
 
-        if(x<20) {
-            System.out.println("뭔가 해보고 있다.");
+        if(x<10) {
+            System.out.println("x가 10 미만이면 출력된다.");
+        } else {
+            System.out.println("그렇지 않을 경우 출력된다.");
+
         }
     }
 }
