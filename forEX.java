413d9d7fb5f96a62c250f3de0d14aaf00115c9b2
yeo taeyang <rhapersephone@gmail.com>
2022-12-14 05:21:47 +0900

    forEX

---
+++ forEX.java
@@ -0,0 +1,14 @@
+public class forEX {
+
+    public static void main(String[] args) {
+        // for (initialization; Boolean Expression; update) {
+        //  statements     
+    //    }
+
+    for (int x=10; x <= 20; x = x+3) {
+        System.out.print("x의 값 : " + x); // 20 미만의 정수를 3씩 더하며 반복해서 출력
+        System.out.print("\n");
+    }
+    }
+    
+}
