ec26c83fecb897878b3335688d2641eef9c6f9f1
yeo taeyang <rhapersephone@gmail.com>
2022-12-15 08:35:20 +0900

    LocalVariableEX

---
+++ localVariableEX.java
@@ -0,0 +1,13 @@
+public class localVariableEX {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println(a + "와 " + b + "의 합은 " + c + "이다.");
+    }
+
+  //  public sum() {
+  //      int a; int b; int c; //지역 변수(Local variable)는 메소드 안에서만 쓸 수 있으므로 오류가 뜬다.
+  //  }
+}
